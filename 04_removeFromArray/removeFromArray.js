e9018b8aad4944389ac3f224f486434c76fc4da4
abatdorf505 <abatdorf505@gmail.com>
2025-03-12 22:18:50 -0400

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    for (value of values) {
+        while (array.includes(value)) {
+            let index = array.indexOf(value);
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
