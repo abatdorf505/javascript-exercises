897e350b859f93afbefefff3a761b7afe01b1bc7
abatdorf505 <abatdorf505@gmail.com>
2025-03-12 22:01:48 -0400

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newArray = string.split('');
+    newArray = newArray.reverse();
+    let newString = newArray.join('');
 
+    return newString;
 };
 
 // Do not edit below this line
