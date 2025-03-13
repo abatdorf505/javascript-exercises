271aa77170740b6af87f3a6cb06e69179d71ac35
abatdorf505 <abatdorf505@gmail.com>
2025-03-13 02:19:16 -0400

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    for (value of [x, y]) {
+        if (
+            typeof value !== "number" ||
+            value < 0 ||
+            Math.floor(value) != value
+        ) {
+            return 'ERROR';
+        }
+    }
 
+    let newSum = 0;
+    if ((x - y) % 2 === 0) {
+        newSum = (x + y) * (Math.abs(x - y) / 2) + ((x + y) / 2);
+    } else {
+        newSum = (x + y) * Math.ceil(Math.abs(x - y) / 2);
+    }
+
+    return newSum;
 };
 
 // Do not edit below this line
