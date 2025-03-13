a39e69aa93637700657373c2d81cdb8a87e1ec15
abatdorf505 <abatdorf505@gmail.com>
2025-03-13 02:45:19 -0400

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
+  if (!Number.isInteger(tempC)) {
+    tempC = parseFloat(tempC.toFixed(1));
+  }
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = tempC * (9 / 5) + 32;
+  if (!Number.isInteger(tempF)) {
+    tempF = parseFloat(tempF.toFixed(1));
+  }
+  return tempF;
 };
 
 // Do not edit below this line
